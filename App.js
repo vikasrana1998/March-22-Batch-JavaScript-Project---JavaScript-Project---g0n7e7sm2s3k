2aab371792e45e4a4d38cd74033afac133955946
Vikash Rana <vikasrana1998@gmail.com>
2022-10-06 17:38:32 +0530

    Submit

--- App.js
+++ App.js
@@ -1,1 +1,24 @@
+const bill = document.querySelector("#bill");
+const people = document.querySelector("#people");
+const res = document.querySelector("#result");
+const tip = document.querySelector("#tip");
 
+function calc(){
+    if(bill.value==""){
+        alert("Please enter Bill Amount!");
+    }
+    else if(tip.value=="none"){
+        alert("Select Service Quality!")
+    }
+    else{
+        if(people.value<=0) people.value = 1;
+        const percentage = +tip.value;
+        const billamount = +bill.value;
+        const share = +people.value;
+
+        const calcTip = ((billamount*percentage)/100)/share;
+        res.innerHTML = calcTip + " Rs. for Each."
+
+    }
+    
+}
